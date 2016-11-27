f7bedd582aca739866f98b65854f54462e5e191e
Hakan Dilek <hakandilek@gmail.com>
2016-11-28 00:16:41 +0100

    retrieve canvas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Platform, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('videoElement') videoElement: ElementRef;
+  @ViewChild('canvasElement') canvasElement: ElementRef;
+  width: number;
+  height: number;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(platform: Platform, public navCtrl: NavController) {
+    this.width = platform.width();
+    this.height = platform.height();
+
+  }
+
+  ngAfterViewInit() {
+    let videoNative = this.videoElement.nativeElement;
+    let canvasNative = this.canvasElement.nativeElement;
+    let ctx = canvasNative.getContext('2d');
+    console.log(`video  : ${videoNative}`);
+    console.log(`canvas : ${canvasNative}`);
+    console.log(`context: ${ctx}`);
+
   }
 
 }

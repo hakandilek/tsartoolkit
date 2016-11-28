5acd9e3b170000289e62793c9d5a85ccf2c20359
Hakan Dilek <hakandilek@gmail.com>
2016-11-28 16:54:29 +0100

    use ARController from jsartoolkit5

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Platform, NavController } from 'ionic-angular';
 
+import { ARController } from 'jsartoolkit5';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -14,7 +16,6 @@ export class HomePage {
   constructor(platform: Platform, public navCtrl: NavController) {
     this.width = platform.width();
     this.height = platform.height();
-
   }
 
   ngAfterViewInit() {
@@ -24,7 +25,16 @@ export class HomePage {
     console.log(`video  : ${videoNative}`);
     console.log(`canvas : ${canvasNative}`);
     console.log(`context: ${ctx}`);
+    let vw = videoNative.width;
+    let vh = videoNative.height;
 
+    let arController = new ARController(vw, vh, `assets/data/camera_para.dat`);
+    arController.onload = () => {
+      arController.debugSetup();
+      setInterval(() => {
+        arController.process(videoNative);
+      }, 13);
+    };
   }
 
 }
